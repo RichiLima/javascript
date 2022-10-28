d41fbeea388c17991dc41c2289c68ff4000a4ec8
Richard Lima <112436267+RichiLima@users.noreply.github.com>
2022-10-28 11:15:55 -0300

    atualização do aula 11

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,1 +1,6 @@
-var vel = 60.km
+var vel = 60.5
+console.log(`A velocidade do seu carro é de ${vel}km/h`)
+if(vel > 60){
+    console.log('Você ultrapassou a velocidade permetida. Multado!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Alemanha'
+console.log(`Vivendo no ${país}`)
+if (país == 'Brasil') {
+    console.log('Você é Brasileiro')
+}else {
+    console.log('você é Estrangeiro')
+}
