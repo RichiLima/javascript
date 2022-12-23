27886b31dd75a3c83f395b1402341785f59d605e
Richard Lima <112436267+RichiLima@users.noreply.github.com>
2022-12-23 11:37:47 -0300

    atualização ex015

--- exercicios/ex015/script.js
+++ exercicios/ex015/script.js
@@ -9,36 +9,48 @@ function verificar() {
         var fsex = document.getElementsByName(`radsex`)
         var idade = ano - Number(fano.value)
         var gênero = ''
-        /*var img = document.createAttribute(`img`)
-        img.setAttribute('id', 'foto')*/
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        
         
         if (fsex[0].checked) {
             gênero = `Homem`
             
             if (idade >= 0 && idade < 10) {
                 //criança
-            }else if (idade < 21) {
-                //jovem
-            } else if (idade <60) {
-                //adulto
-            } else {
-                //idoso
-            }
-        }else if (fsex[1].checked) {
-            gênero = `Mulher`
-            if (idade >= 0 && idade < 10) {
-                //criança
+                img.setAttribute('src', "menino.jpg")
             } else if (idade < 21) {
                 //jovem
+                img.setAttribute('src', 'jpvem-homem.jpg')
             } else if (idade <60) {
                 //adulto
+                img.setAttribute('src', 'homem-adulto.jpg')
             } else {
                 //idoso
+                img.setAttribute('src', 'homem-velho.jpg')
+            }
+     
+        }else if (fsex[1].checked) {
+            gênero = `Mulher`
+
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'menina.jpg')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'jovem-mulher.jpg')
+            } else if (idade <60) {
+                //adulto
+                img.setAttribute('src', 'mulher-adulta.jpg')
+            } else {
+                //idoso
+                img.setAttribute('src', 'mulher-velha.jpg')
             }
             
-            res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
-   
-        }
+         }   
         
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos`  
+        res.appendChild(img) 
+        res.body.style.textalingn = 'center'
     }
 }
